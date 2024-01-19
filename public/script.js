25b373d1ad0910c707a23abef474f6a7c432ab06
Ameerali <ameeralipm317@gmail.com>
2024-01-19 21:40:36 +0530

    Added basic DB funtions

--- public/script.js
+++ public/script.js
@@ -1,3 +1,20 @@
+document.addEventListener("DOMContentLoaded", init, false);
+
+let globalRef;
+function init(){
+    firebase.initializeApp(firebaseConfig);
+    database = firebase.database();
+    globalRef = database.ref("users");
+    userRef = globalRef.child("user1");
+
+    document.getElementById("addIncome").addEventListener("click",addIncome,false);
+    document.getElementById("addExpense").addEventListener("click",addExpense,false);
+    document.getElementById("extras").addEventListener("click",getBalance,false);
+
+    console.log("init");
+}
+
+
 const firebaseConfig = {
     apiKey: "AIzaSyDwZ4DRzgDSKfei6jd6_u4sJf3cVECV3nE",
     authDomain: "cashcraftfinances.firebaseapp.com",
@@ -9,17 +26,34 @@ const firebaseConfig = {
     measurementId: "G-1PWK0RT8N6"
 };
 
-firebase.initializeApp(firebaseConfig);
-var database = firebase.database();
+globalRef.on('value', getBalance);
+function getBalance(){
+    userRef.on('value', (snapshot) => {
+        const data = snapshot.val();
+        console.log("DB updates", data);
+        document.getElementById("balance").innerHTML = data.balance;
+    });    
+}
+
+
+function addIncome(){
+    
+}
+
 
 function addExpense(){
-    var userRef = database.ref("User");
 
-    var expense = {
-        Name : "Ami",
-        Balance : 20
-    };
-
-    userRef.push(expense);
-    console.log("Expense updated"); 
+    var expense = document.getElementById("expense").value;
+    userRef.update({
+        balance: expense
+      })
+        .then(() => {
+          console.log("New balance = ",expense);
+        })
+        .catch((error) => {
+          console.error("Error updating balance:", error);
+        }); 
+    
+    getBalance();
 }
+
